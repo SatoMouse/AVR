05e3199e59d6da03f44fd3b17d7c81337bc963c1
SatoMouse <41887388+SatoMouse@users.noreply.github.com>
2018-08-01 00:29:41 +0900

    Add files via upload

---
+++ attiny13a_servo/attiny13a_servo.c
@@ -0,0 +1,39 @@
+#define F_CPU 1600000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define OFF_POS 8
+#define ON_POS 10
+//-90Åã  4
+// 90Åã 10
+
+ISR(PCINT0_vect){
+	OCR0B = ON_POS;
+	_delay_ms(600);
+	GIFR |= 0<<5;
+	OCR0B = OFF_POS;
+	_delay_ms(600);
+	OCR0B = 0;
+}
+
+int main(void)
+{
+	DDRB |= (0 << PINB4) | (1 << PINB1);
+	
+	GIMSK |= (1 << 5);
+	PCMSK = 0x10;
+
+	TCCR0A |= 1<<WGM01 | 1<<WGM00 | 1<<COM0B1;
+	TCCR0B |= 1<<WGM02 | 1<<CS02;// ï™é¸î‰256
+	OCR0A = 93; // 1.2MHz/50Hz/256-1=94 - 1 = 93
+	
+	OCR0B = OFF_POS;
+	_delay_ms(600);
+	OCR0B = 0;
+	
+	sei();
+
+	while (1){}
+}
